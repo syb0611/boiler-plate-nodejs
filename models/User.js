193db7bf2410f33458899fa84e32a9b07c150a30
seo <sybin611@gmail.com>
2020-12-11 18:48:24 +0900

    bcrypt password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,11 @@
 //mongoose 모듈 가져오기
 const mongoose = require('mongoose')
 
+//bycrypt 모듈 가져오기
+const bycrypt = require('bcrypt');
+//saltRounds 값 설정
+const saltRounds = 10;
+
 //mongoose 이용해서 schema 생성
 const userSchema = mongoose.Schema({
     name: {
@@ -32,6 +37,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+//저장하기 전에 수행
+userSchema.pre('save', function(next) {
+
+    var user = this;
+
+    //비밀번호 바뀔 때만 적용되도록 조건
+    if(user.isModified('password')) {
+        //비밀번호 암호화
+        bycrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err);
+
+            bycrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err);
+
+                user.password = hash;
+                next();
+            });
+        });
+    }
+});
 
 //schema를 model로 감싸주기
 const User = mongoose.model('User', userSchema)
