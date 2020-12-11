7442beb22cbe8353ab050e21e0338f90c7acce01
seo <sybin611@gmail.com>
2020-12-11 15:03:52 +0900

    add body-parser, register route

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser')           //Body-Parser
+const {User} = require("./models/User")             //User 모듈 가져오기
+
+app.use(bodyParser.urlencoded({extended: true}))    //application/x-www-form-urlencoded
+app.use(bodyParser.json())                          //application/json
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://seo:0000@boiler-plate.mlzqt.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
@@ -12,8 +18,22 @@ mongoose.connect('mongodb+srv://seo:0000@boiler-plate.mlzqt.mongodb.net/test?ret
    .catch(err => console.log(err))
 
 
+//기본 Route
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+//회원가입 Route
+app.post('/register', (req, res) => {
+
+    //회원가입 시 필요한 정보들을 client에서 가져와서 DB 저장
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
 })
 
 app.listen(port, () => {
