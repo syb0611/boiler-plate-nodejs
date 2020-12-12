1122f10a68eb4623cb32b798709129009876caa6
seo <sybin611@gmail.com>
2020-12-12 18:17:36 +0900

    add Logout Route, modify auth.js(cookie->cookies)

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ let auth = (req, res, next) => {
     //인증 처리를 하는 곳
 
     //1. 클라이언트 쿠키에서 token 가져옴
-    let token = req.cookie.x_auth;
+    let token = req.cookies.x_auth;
 
     //2. token 복호화 한 후 유저 찾음
     User.findByToken(token, (err, user) => {
