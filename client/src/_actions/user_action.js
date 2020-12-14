5c8927919b9ce91f8197a37cb86ee23b89faccdf
seo <sybin611@gmail.com>
2020-12-14 16:02:55 +0900

    Login Page

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import {
+    LOGIN_USER
+} from './types';
+
+export function loginUser(dataTosubmit) {
+
+    const request = axios.post('/api/users/login', dataTosubmit)
+        .then(response => response.data);
+
+    //request를 reducer로 넘겨줌
+    return {
+        type: LOGIN_USER,
+        payload: request
+    }
+}
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Axios from 'axios';
+import {useDispatch} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {loginUser} from '../../../_actions/user_action';
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    //State
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+
+    //Handler
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    };
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        };
+
+        //redux action
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess) {
+                    props.history.push('/'); //랜딩페이지로 이동
+                } else {
+                    alert('Error');
+                }
+            })
+    };
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
 
-export default LoginPage;
+export default withRouter(LoginPage);
