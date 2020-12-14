5b969d68ee4822da6175831ba2bd9e84247c71bf
seo <sybin611@gmail.com>
2020-12-14 16:33:18 +0900

    Logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if(response.data.success) {
+                    props.history.push('/login');
+                } else {
+                    alert('로그아웃 실패');
+                }
+            })
+    };
+
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                Logout
+            </button>
         </div>
     )
 }
