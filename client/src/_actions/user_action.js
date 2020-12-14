3428cbe6dfcc0ee0afa6b18887ec6648ed43f1ef
seo <sybin611@gmail.com>
2020-12-14 16:25:28 +0900

    Register Page

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from './types';
 
 export function loginUser(dataTosubmit) {
@@ -11,6 +12,17 @@ export function loginUser(dataTosubmit) {
     //request를 reducer로 넘겨줌
     return {
         type: LOGIN_USER,
+        payload: request
+    }
+}
+
+export function registerUser(dataTosubmit) {
+
+    const request = axios.post('/api/users/register', dataTosubmit)
+        .then(response => response.data);
+
+    return {
+        type: REGISTER_USER,
         payload: request
     }
 }
